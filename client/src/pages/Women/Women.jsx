6e7eb05e8c85835c62ffaa18fdb5978a3279cc4e
ViniSanoDev <devmailsano@gmail.com>
2025-05-29 13:04:48 +0100

    Update Women.jsx

--- client/src/pages/Women/Women.jsx
+++ client/src/pages/Women/Women.jsx
@@ -17,7 +17,7 @@ const Women = () => {
   useEffect(() => {
     const loadProducts = async () => {
       try {
-        const response = await axios.get('/api/new-in/'');
+        const response = await axios.get('/api/new-in/');
         setProducts(response.data);
         setFilteredProducts(response.data);
       } catch (err) {
