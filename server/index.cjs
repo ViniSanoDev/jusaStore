4f67904b29e373efea47f7ac25758afbb50c6bbc
ViniSanoDev <devmailsano@gmail.com>
2025-05-29 13:54:13 +0100

    Update index.cjs

--- server/index.cjs
+++ server/index.cjs
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-    origin: "https://jusastore.onrender.com", 
+    origin: "*", 
     credentials: true,
     methods: 'GET,POST,PUT,DELETE',
   })
